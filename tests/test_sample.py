df30838831171f5b0a5a56a586d5dc3b58bf635f
laban254 <labanrotich6544@gmail.com>
2024-09-08 23:19:03 +0300

    test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,6 +4,6 @@ import pytest
 def test_addition():
     assert 1 + 1 == 2
 
-# Another sample test functiion --0-
+# Another sample test functiion --09-
 def test_subtraction():
     assert 5 - 3 == 2
